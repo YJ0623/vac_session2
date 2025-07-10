fc5bae00cd3a96d62f435bb931beffe342a7bc4c
YJ <hghk0046@gmail.com>
2025-07-10 20:09:38 +0900

    수정해서 커밋

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import './index.css';
 import App from './App.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import {Analytics} from '@vercel/analytics';
-import {SpeedInsights} from '@vercel/speed-insights';
+import {SpeedInsights} from '@vercel/speed-insights/react';
 
 const queryClient = new QueryClient();
 
@@ -13,7 +12,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <Analytics/>
       <SpeedInsights/>
       <ReactQueryDevtools initialIsOpen={true} />
     </QueryClientProvider>
